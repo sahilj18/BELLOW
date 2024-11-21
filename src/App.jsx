1e812d126b0e3c8366befad6bc496c0b0dbd3891
sahilj18 <sahilmernj18@proton.me>
2024-11-21 08:06:50 +0000

    saveeeeee

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import {useState as usestate} from 'react';
 import items from './assets/items.json';
 import Bellow from './Bellow';
 
 function App() {
+  const [lastclicked ,setlastclicked] =usestate ('None');
   return (
     <div className="flex justify-center items-center min-h-screen my-10">
       <Bellow
